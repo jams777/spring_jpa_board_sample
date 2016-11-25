a77ecc3345b989b0459a200b5a2225a1e2d2837e
JiamSeo <jams7777@gmail.com>
2016-11-25 20:15:11 +0900

    Initial commit

---
+++ src/main/java/com/jams7777/study/ddd/SpringJpaBoardSampleApplication.java
@@ -0,0 +1,12 @@
+package com.jams7777.study.ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaBoardSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaBoardSampleApplication.class, args);
+	}
+}
